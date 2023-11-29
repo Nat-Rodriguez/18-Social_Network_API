55dbeb6a3955ec87213f7b4b0dc0c94527cc0dab
Nat <n3rodriguez2001@gmail.com>
2023-11-28 20:43:34 -0500

    added some routes

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.send('Something is wrong with the route!'));
+
+module.exports = router;
